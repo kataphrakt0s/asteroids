4574f26ca2367e10f8c020a7a6ad14f74e1522be
kataphrakt0s <cam.m.muir@gmail.com>
2025-07-31 14:44:55 -0600

    added:   player.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -12,8 +13,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         screen.fill((0, 0, 0))  # Fill the screen with black
+
+        player.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
